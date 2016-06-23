afdeba75f72fdd4ef44347beb65724b8569f019b
lehuohuo <lehuohuo016@gmail.com>
2016-06-23 10:29:50 +0800

    增加信息模板

---
+++ src/main/java/com/fengyu/system/dao/MessageTemplatesDao.java
@@ -0,0 +1,8 @@
+package com.fengyu.system.dao;
+import com.fengyu.common.persistence.CrudDao;
+import com.fengyu.common.persistence.annotation.MyBatisDao;
+import com.fengyu.system.entity.MessageTemplates;
+@MyBatisDao
+public interface MessageTemplatesDao extends CrudDao<MessageTemplates>{
+
+}
