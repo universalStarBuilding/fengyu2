23e2fac358d56f302b00e40f3fdbfdcedd890e16
wudonglin <296734078@qq.com>
2016-06-27 21:01:27 +0800

    订单列表

--- src/main/java/com/fengyu/modules/service/project/msg/OrderService.java
+++ src/main/java/com/fengyu/modules/service/project/msg/OrderService.java
@@ -31,7 +31,28 @@ public class OrderService {
         return result ;
     }
 
+    /**
+     * 查询所有订单列表
+     * @param order
+     * @return
+     */
+    public SearchResult getOrderList(Order order){
+        SearchResult<Order> result = new SearchResult<>();
+        result.setTotalRows(orderMapper.orderPage(order));
+        result.setRows(orderMapper.getOrderList(order));
+        return result;
+    }
 
-
-
+    /**
+     * 查询待付款的订单
+     * @param order
+     * @return
+     */
+    public SearchResult getPayment(Order order){
+        order.setOrderState("0");
+        SearchResult<Order> result = new SearchResult<>();
+        result.setTotalRows(orderMapper.orderPage(order));
+        result.setRows(orderMapper.getStateOrderList(order));
+        return result;
+    }
 }
